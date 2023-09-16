7bbd783396cb13927607768dbb395c3f1e394b2b
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2023-09-15 22:42:16 -0400

    final

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Frank Fu Student ID: 126609197 Date: Sept 15 2023
 
-app.get('/', (req, res) => {
-    res.send('Frank Fu 126609197!');
-})
+*  Online (Cyclic) URL:  https://weary-boot-deer.cyclic.cloud/
+*
+********************************************************************************/
+
+
+const app = require('./app')
+
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`)
 })
